49ccc18294ea6a6082c802835d76fe082ff86754
NikaBotchorishvili <nikabotchorishvili@gmail.com>
2023-08-17 14:51:46 +0400

    server deployement

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import cors from "cors";
 
 const PORT = process.env.PORT
 
@@ -12,6 +11,6 @@ app.listen(PORT, () => {
     console.log(`listening on PORT:${PORT}`)
 })
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
     res.send("Hello World");
 })
