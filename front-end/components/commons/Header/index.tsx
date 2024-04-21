c9ba497adf0beb78dac05a8d75c4de90df7927a2
Nika Botchorishvili <nikabotchorishvili@gmail.com>
2024-04-21 20:10:59 +0400

    feat[front-end]: Fixed the background pattern for the header

--- front-end/components/commons/Header/index.tsx
+++ front-end/components/commons/Header/index.tsx
@@ -1,11 +1,21 @@
 import Link from "next/link";
 import ThemeSwitcher from "./components/ThemeSwitcher";
-Link
+import Image from "next/image";
+import DesktopPattern from "../../../public/desktop/bg-pattern-header.svg";
 function Header() {
 	return (
-		<header className="flex w-screen md:h-[160px] mobileHeader h-[136px] md:desktopHeader">
+		<header className="flex w-screen relative  md:h-[160px]  h-[136px]">
+			<Image
+				className="absolute w-full h-full -z-10"
+				alt=""
+
+				src={DesktopPattern}
+			/>
 			<div className="flex w-[70%] justify-between mx-auto items-start pt-5 sm:pt-0   sm:items-center">
-				<Link href="/" className="text-4xl text-white font-bold leading-none">
+				<Link
+					href="/"
+					className="text-4xl text-white font-bold leading-none"
+				>
 					devjobs
 				</Link>
 				<ThemeSwitcher />
